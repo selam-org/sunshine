4d6bf8f3f2c9427feebb36679e0c1b604db6bd7f
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2024-08-20 12:06:41 -0400

    fix export excel file

--- src/pages/SearchOrders.js
+++ src/pages/SearchOrders.js
@@ -15,59 +15,62 @@ const { TabPane } = Tabs;
 const OrdersPage = () => {
   const orders = useSelector(getOrders);
   const handleExport = () => {
+    
+  
     // Format the orders data for export
     const formattedData = orders.map(order => ({
-      "Invoice": "SE001-3", // Default value
-      "Confirmation No": "524475046535", // Default value
-      "Agency": "SE001", // Default value
-      "Date": moment(order.created_at).format("MM/DD/YYYY h:mm:ss A"), // Mapping created_at to Date
-      "Send Currency": "Dollar", // Default value
-      "Received Currency": "Ethiopian Birr", // Default value
-      "Rate Change Receiver": order.rate || 50.0, // Use order.rate or default value 50.0
-      "Net Amount Receiver": 100.0, // Default value
-      "Fee": order.commission || 4.0, // Use order.commission or default value 4.0
-      "Total": order.total_usd || 104.0, // Use order.total_usd or default value 104.0
-      "Payment Type": "Cash", // Default value
-      "Total Pay Receiver": order.total_birr || 5000.0, // Use order.total_birr or default value 5000.0
-      "Sender": `${order.sender.first_name} ${order.sender.last_name}` || "LAMEK MERSO", // Use sender name or default
-      "Sender Phone": order.sender.phone_number || "", // Use sender phone number or default
-      "Sender Address": "13103 BRITTANY DR", // Default value
-      "Sender City": "SILVER SPRING, Maryland-", // Default value
-      "Sender State": "Maryland", // Default value
-      "Sender Zip": "", // Default value
-      "Birthday Sender": "08/01/1987", // Default value
-      "Sender SSN": "", // Default value
-      "Name Type Id Sender": "Driver License", // Default value
-      "Number Id Sender": "620488013025", // Default value
-      "Sender State Identification": "Maryland", // Default value
-      "Sender Country Identification": "United States", // Default value
-      "Receiver": `${order.receiver.first_name} ${order.receiver.last_name}` || "SHAMSIA NURBEGIE", // Use receiver name or default
-      "Receiver Phone": order.receiver.phone_number || "", // Use receiver phone or default
-      "Receiver Address": "", // Default value
-      "Receiver City": "ADDIS ABABA GPO", // Default value
-      "Receiver State": "Ethiopia", // Default value
-      "Receiver Country": "Ethiopia", // Default value
-      "Payee Reference": "SE001-3", // Default value
-      "Employee Code": "Marefat", // Default value
-      "Payee Agency": "Pa001 ehtiopia payee partner", // Default value
-      "Point of Payment": "ehtiopia payee partner", // Default value
-      "Mode Pay Receiver": "BANK DEPOSIT", // Default value
-      "Bank": "Commercial Bank of Ethiopia", // Default value
-      "Bank Account": order.receiver.default_bank.account || "1000202444133", // Use receiver bank account or default
-      "Id Sender": "208495", // Default value
-      "Notes Receiver": "", // Default value
-      "Company": "Selam Express", // Default value
-      "ID Branch": "SE001", // Default value
-      "Name Agency": "Agency Saleemexpress, United States", // Default value
-      "Address Agency": "8205 fenton street", // Default value
-      "City Agency": "SILVER SPRING,Maryland-20910", // Default value
-      "State Agency": "Maryland", // Default value
-      "Zip_Agency": 20910, // Default value
-      "Id Country Transmitter": "United States", // Default value
-      "Id Country National": "United States", // Default value
-      "Sender Sex": "M", // Default value
-      "Citizenship": "United States", // Default value
-      "Send Date": moment(order.created_at).format("MM/DD/YYYY h:mm:ss A"), // Mapping created_at to Send Date
+      "Invoice": "SE001-3",
+      "Confirmation No": "524475046535",
+      "Agency": "SE001",
+      "Date": moment().format("MM/DD/YYYY h:mm:ss A"),
+      "Send Currency": "Dollar",
+      "Received Currency": "Ethiopian Birr",
+      "Rate Change Receiver": order.rate || 0.0,
+      "Net Amount Receiver": order.total_birr || 0.0,
+      "Fee": order.commission || 0.0,
+      "Total": order.total_usd || 0.0,
+      "Payment Type": "Cash",
+      "Total Pay Receiver": 0.0,
+      "Sender": `${order.sender.first_name} ${order.sender.last_name}`,
+      "Sender Phone": order.sender.phone_number,
+      "Sender Address": "13103 BRITTANY DR",
+      "Sender City": "SILVER SPRING, Maryland-",
+      "Sender State": "Maryland",
+      "Sender Zip": "20910",
+      "Birthday Sender": "08/01/1987",
+      "Sender SSN": "123-45-6789",
+      "Name Type Id Sender": "Driver License",
+      "Number Id Sender": "620488013025",
+      "Sender State Identification": "Maryland",
+      "Sender Country Identification": "United States",
+      "Receiver": `${order.receiver.first_name} ${order.receiver.last_name}`,
+      "Receiver Phone": order.receiver.phone_number,
+      "Receiver Address": "Unknown",
+      "Receiver City": "ADDIS ABABA GPO",
+      "Receiver State": "Ethiopia",
+      "Receiver Country": "Ethiopia",
+      "Status": order.status,
+      "Payee Reference": "SE001-3",
+      "Employee Code": "Marefat",
+      "Payee Agency": "Pa001 ehtiopia payee partner",
+      "Point of Payment": "ehtiopia payee partner",
+      "Mode Pay Receiver": "BANK DEPOSIT",
+      "Bank": order.receiver.default_bank.bank,
+      "Bank Account": order.receiver.default_bank.account,
+      "Id Sender": "208495",
+      "Notes Receiver": "No additional notes",
+      "Company": "Selam Express",
+      "ID Branch": "SE001",
+      "Name Agency": "Agency Saleemexpress, United States",
+      "Address Agency": "8205 fenton street",
+      "City Agency": "SILVER SPRING,Maryland-20910",
+      "State Agency": "Maryland",
+      "Zip_Agency": 20910,
+      "Id Country Transmitter": "United States",
+      "Id Country National": "United States",
+      "Sender Sex": "M",
+      "Citizenship": "United States",
+      "Send Date": moment().format("MM/DD/YYYY h:mm:ss A")
     }));
   
     // Create a new worksheet
@@ -78,9 +81,10 @@ const OrdersPage = () => {
     XLSX.utils.book_append_sheet(wb, ws, "Orders");
   
     // Generate Excel file and trigger download
-    XLSX.writeFile(wb, "Orders_Report.xlsx");
+    const fileName = `Order_${moment().format("YYYYMMDD_HHmmss")}.xlsx`;
+    XLSX.writeFile(wb, fileName);
   };
-  
+
   return (
     <div style={{ padding: "24px" }}>
       <Card
